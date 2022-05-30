5e41719ee2030d58892244957b847389b79c744b
SvetlanaZt <svetlana.zt23@gmail.com>
2022-05-30 12:54:05 +0300

    dz

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/main.scss';
 import axios from "axios";
+// import { addPost } from './api/api'
 import template from './template/img.hbs';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
@@ -7,12 +8,21 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
 
-const form = document.querySelector('.search-form')
+const form = document.getElementById('search-form')
 const list = document.querySelector('.gallery')
 const loaMore = document.querySelector('.load-more')
 
 form.addEventListener('submit', onClick)
 loaMore.addEventListener('click', onLoadMore)
+
+ var lightbox = new SimpleLightbox('.gallery a', { 
+    captionDelay: 250,
+   close: true,
+   enableKeyboard: true,
+   swipeClose: true,
+    showCounter: true,
+    docClose: true,
+ });
 
 let element = ''
 
@@ -20,17 +30,23 @@ function onClick(evt) {
   evt.preventDefault()
   element = evt.target.searchQuery.value;
   addPost(element)
+  if (element === '') {
+    list.innerHTML='';
+  }
 }
 
- function addPost(element) {
-  axios.get(`https://pixabay.com/api/?key=27593134-a882df11ea431345edf986e72&q=${element}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`)
-    .then(function (response) {
-      renderImg(response)
-    })
-    .catch(function (error) {
+function addPost(element) {
+  try {
+    const key = '27593134-a882df11ea431345edf986e72'
+     axios.get(`https://pixabay.com/api/?key=${key}&q=${element}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`)
+      .then(function (response) {
+        renderImg(response)
+      })
+  }
+  catch (error) {
       // handle error
       console.log(error);
-    })
+    }
 }
 
 function renderImg(name) {
@@ -42,28 +58,14 @@ function renderImg(name) {
   }
 
 }
- 
+
 let currentPage = 1
 function onLoadMore() {
   currentPage += 1
-   axios.get(`https://pixabay.com/api/?key=27593134-a882df11ea431345edf986e72&q=${element}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`)
-    .then(function (response) {
-      renderImg(response)
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-  
+  addPost(currentPage)
 }
 
- var lightbox = new SimpleLightbox('.gallery a', { 
-    captionDelay: 250,
-   close: true,
-   enableKeyboard: true,
-   swipeClose: true,
-    showCounter: true,
- });
+
 
 
 
