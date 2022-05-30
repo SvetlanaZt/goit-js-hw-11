142dc8593a1f9e828b823cd878b2dc6e66b64232
SvetlanaZt <svetlana.zt23@gmail.com>
2022-05-30 13:43:11 +0300

    dz

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ const loaMore = document.querySelector('.load-more')
 form.addEventListener('submit', onClick)
 loaMore.addEventListener('click', onLoadMore)
 
+loaMore.classList.add('is-hidden')
+
  var lightbox = new SimpleLightbox('.gallery a', { 
     captionDelay: 250,
    close: true,
@@ -34,11 +36,11 @@ function onClick(evt) {
     list.innerHTML='';
   }
 }
-
+const perPage = '40'
 function addPost(element) {
   try {
     const key = '27593134-a882df11ea431345edf986e72'
-     axios.get(`https://pixabay.com/api/?key=${key}&q=${element}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`)
+     axios.get(`https://pixabay.com/api/?key=${key}&q=${element}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=${perPage}`)
       .then(function (response) {
         renderImg(response)
       })
@@ -50,19 +52,30 @@ function addPost(element) {
 }
 
 function renderImg(name) {
-  if (name.data.total === 0) {
+  if (name.data.totalHits > perPage) {
+    loaMore.classList.remove('is-hidden')
+  }
+  if (name.data.totalHits === 0) {
     Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
   } else {
     const addImg = template(name);
     return list.innerHTML = addImg;
   }
-
+  
+lightbox.refresh();
 }
 
 let currentPage = 1
 function onLoadMore() {
   currentPage += 1
   addPost(currentPage)
+  // const totalPages = Math.floor(data.totalHits / perPage)
+  // console.log(totalPages)
+  //     if (currentPage > totalPages) {
+  //       loaMore.classList.add('is-hidden') 
+  //       Notify.failure("We're sorry, but you've reached the end of search results.")
+  //     }
+  
 }
 
 
